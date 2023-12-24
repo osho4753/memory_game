f8ce3a1556bf8b98efb254179951ba110faafb87
osho4753 <ramazanmamanov840@gmail.com>
2023-12-25 00:33:01 +0100

    timer implement

--- app.js
+++ app.js
@@ -12,33 +12,92 @@ startButton.textContent = 'Start Game'
 headElement.appendChild(endButton)
 headElement.appendChild(startButton)
 
-let difficulcy = document.createElement('h1')
+let lvl = document.createElement('h1')
 
 endButton.style.display = 'none'
 
+let time = document.createElement('h2')
+let timer
+function bigTimer(sec, min) {
+  timer = setInterval(function () {
+    switch (min) {
+      case 3:
+        time.style.fontSize = '75px'
+        time.style.color = 'red'
+        time.textContent = 'Ready'
+        break
+      case 2:
+        time.textContent = 'Steady'
+        break
+      case 1:
+        time.style.color = 'green'
+        time.textContent = 'GO!'
+        break
+    }
+    min--
+    if (min < 0) {
+      image.map((e) => (e.style.display = 'inline'))
+      time.style.color = 'black'
+      time.style.fontSize = '25px'
+      time.textContent = sec
+      sec--
+      sec === 0 &&
+        setTimeout(function () {
+          end()
+          alert('YOU lose!')
+        }, 500)
+
+      if (sec < 0) {
+        clearInterval(timer)
+      }
+      if (sec < 4) {
+        time.style.fontSize = '35px'
+        time.style.color = 'red'
+      }
+    }
+  }, 1000)
+}
+
+const end = () => {
+  image.map((e) => e.remove())
+  startButton.style.display = 'inline'
+  endButton.style.display = 'none'
+  lvl.remove()
+  time.remove()
+  time.textContent = null
+  clearInterval(timer)
+  firstImage = 2
+  secondImage = 1
+}
 const memoryGame = (level) => {
+  clearInterval(timer)
+
   image = []
   hasImage = []
-
   startButton.style.display = 'none'
   endButton.style.display = 'inline'
-  headElement.appendChild(difficulcy)
-  difficulcy.textContent = `${level} level`
+  const imageContainer = document.createElement('div')
+
+  headElement.appendChild(lvl)
+  headElement.appendChild(time)
+  lvl.textContent = `${level} level`
   let numberOfCards
   let numberOfImages
-
   switch (level) {
     case 1:
       numberOfCards = 5
       numberOfImages = 3
+      bigTimer(20, 3)
       break
     case 2:
       numberOfCards = 7
       numberOfImages = 4
+      bigTimer(30, 3)
       break
     case 3:
       numberOfCards = 9
       numberOfImages = 5
+      bigTimer(40, 3)
       break
   }
 
@@ -49,7 +108,9 @@ const memoryGame = (level) => {
     image[i] = document.createElement(`img`)
     image[i].setAttribute('alt', `${i + 1}`)
     hasImage[i] = `./images/${z + 1}.png`
+    imageContainer.appendChild(image[i])
   }
+  image.map((e) => (e.style.display = 'none'))
 
   let newSet = image.length
 
@@ -73,9 +134,8 @@ const memoryGame = (level) => {
   image.map((e) => e.setAttribute('width', '130px'))
   image.map((e) => e.setAttribute('height', '150px'))
   image.map((e) => e.setAttribute('src', './images/cart.png'))
-  for (let i = 0; i < image.length; i++) {
-    headElement.appendChild(image[i])
-  }
+
+  headElement.appendChild(imageContainer)
 
   image.map((e) =>
     e.addEventListener('click', async function () {
@@ -112,7 +172,8 @@ const memoryGame = (level) => {
         setTimeout(function () {
           startButton.style.display = 'inline'
           endButton.style.display = 'none'
-          difficulcy.remove()
+          lvl.remove()
+          end()
           alert('You win!!! Salam Aleikum')
         }, 500)
       newSet == 0 &&
@@ -120,21 +181,15 @@ const memoryGame = (level) => {
         setTimeout(function () {
           level += 1
           memoryGame(level)
-
           alert('Go next Round')
         }, 500)
     })
   )
 }
-endButton.addEventListener('click', function () {
-  image.map((e) => e.remove())
-  startButton.style.display = 'inline'
-  endButton.style.display = 'none'
-  difficulcy.remove()
-  firstImage = 2
-  secondImage = 1
-})
 
+endButton.addEventListener('click', function () {
+  end()
+})
 startButton.addEventListener('click', async function () {
   memoryGame(1)
 })
